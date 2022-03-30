f0af6b81fd8511b93538d9d6b01a68bb849490fa
yujeung <98577125+yujeung@users.noreply.github.com>
2022-03-30 10:05:45 +0900

    Update skill_script.js

--- js/skill_script.js
+++ js/skill_script.js
@@ -42,7 +42,7 @@
             run_duration -= 1000;
             _left.css({
               'clip': 'rect(0, 150px, 150px, 75px)',
-              'background': '#B0DAB9'
+              'background': '#edee4'
             });
             return _left.animate({
               circle_rotate: deg
